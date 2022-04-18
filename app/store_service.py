0abbd7c21ca651b8fa5a47372978141a5bd4f3e9
djacoby <jacoby934@gmail.com>
2022-04-17 23:29:35 -0400

    refactor db service/ write store service functions

---
+++ app/store_service.py
@@ -0,0 +1,20 @@
+from db import execute_query
+from store_queries import get_all_stores_query, get_store_by_id_query, get_stores_by_district_query
+
+
+def get_all_stores():
+    query = get_all_stores_query()
+    result = execute_query(query)
+    return result
+
+
+def get_store_by_id(store_id):
+    query = get_store_by_id_query(store_id)
+    result = execute_query(query)
+    return result
+
+
+def get_store_by_district(district):
+    query = get_stores_by_district_query(district)
+    result = execute_query(query)
+    return result
