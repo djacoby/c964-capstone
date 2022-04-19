c5efcef3c940880b8fb6c81a4f0dea1d3f8531de
djacoby <jacoby934@gmail.com>
2022-04-18 21:22:13 -0400

    style cleanup

--- app/app.py
+++ app/app.py
@@ -1,6 +1,16 @@
 from json import dumps
-from flask import Flask, jsonify
-from store_service import get_all_stores, get_store_by_id, get_store_by_district
+from flask import Flask, jsonify, request
+
+from store_service import (
+    get_all_stores,
+    get_store_by_id,
+    get_store_by_district,
+)
+
+from transaction_service import (
+    get_all_transactions_for_store,
+    get_transactions_for_store_date_range,
+)
 
 app = Flask(__name__)
 
@@ -33,5 +43,18 @@ def store_by_id(store_id):
 @app.route("/api/v1/store/district/<int:district>", methods=["GET"])
 def store_by_district(district):
     result = get_store_by_district(district)
+    headers = {"Content-Type": "application/json"}
+    return jsonify({"result": result}), 200, headers
+
+
+@app.route("/api/v1/store/<int:store_id>/transaction", methods=["GET"])
+def transactions_by_store_id(store_id):
+    if "start_date" in request.args and "end_date" in request.args:
+        start_date = request.args["start_date"]
+        end_date = request.args["end_date"]
+        result = get_transactions_for_store_date_range(
+            store_id, start_date, end_date)
+    else:
+        result = get_all_transactions_for_store(store_id)
     headers = {"Content-Type": "application/json"}
     return jsonify({"result": result}), 200, headers
--- app/store_service.py
+++ app/store_service.py
@@ -1,5 +1,10 @@
 from db import execute_query
-from store_queries import get_all_stores_query, get_store_by_id_query, get_stores_by_district_query
+
+from store_queries import (
+    get_all_stores_query,
+    get_store_by_id_query,
+    get_stores_by_district_query,
+)
 
 
 def get_all_stores():
