af507f5fccee76f4c52701d15bde584f51454d5e
djacoby <jacoby934@gmail.com>
2022-04-18 21:21:55 -0400

    refactor for parameterized queries

--- app/db.py
+++ app/db.py
@@ -5,6 +5,6 @@ engine = create_engine('postgresql://postgres@localhost:5432/capstone')
 
 def execute_query(query):
     db = engine.connect()
-    result = db.execute(query)
+    result = db.execute(*query)
     db.close()
     return [dict(row) for row in result]
--- app/store_queries.py
+++ app/store_queries.py
@@ -2,41 +2,41 @@ def get_all_stores_query():
     """
     Returns a query that returns all stores.
     """
-    return f"""
+    return """
         SELECT
           id
           ,city
           ,state
           ,district
         FROM store;
-    """
+    """, ()
 
 
 def get_store_by_id_query(store_id):
     """
     Returns a query that returns a single store by id.
     """
-    return f"""
+    return """
         SELECT
           id
           ,city
           ,state
           ,district
         FROM store
-        WHERE id = '{store_id}'
-    """
+        WHERE id = %s;
+    """, store_id
 
 
 def get_stores_by_district_query(district):
     """
     Returns a query that returns all stores for a district.
     """
-    return f"""
+    return """
         SELECT
           id
           ,city
           ,state
           ,district
         FROM store
-        WHERE district = '{district}'
-    """
+        WHERE district = %s;
+    """, district
