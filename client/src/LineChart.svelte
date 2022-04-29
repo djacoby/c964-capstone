3b9ab81b87ca6f263c74046439cee7822fe25efe
djacoby <jacoby934@gmail.com>
2022-04-28 20:18:38 -0400

    finish client

---
+++ client/src/LineChart.svelte
@@ -0,0 +1,58 @@
+<script>
+  import {
+    Chart,
+    LineElement,
+    PointElement,
+    LineController,
+    CategoryScale,
+    LinearScale,
+    Legend,
+    Title,
+    Tooltip,
+    SubTitle,
+  } from 'chart.js';
+
+  Chart.register(
+    LineElement,
+    PointElement,
+    LineController,
+    CategoryScale,
+    LinearScale,
+    Legend,
+    Title,
+    Tooltip,
+    SubTitle
+  );
+
+  import { getLineChartConfig } from './chart-config';
+
+  export let forecast;
+  let chart;
+  let ctx;
+
+  $: {
+    ctx = document.getElementById('lineChart');
+
+    if (ctx && forecast) {
+      renderChart();
+    }
+  }
+
+  function renderChart() {
+    if (chart) {
+      chart.destroy();
+    }
+
+    const chartConfig = getLineChartConfig(forecast, 'line');
+
+    chart = new Chart(ctx, chartConfig);
+  }
+</script>
+
+<main>
+  <div class="my-4 w-100">
+    <canvas id="lineChart" />
+  </div>
+</main>
+
+<style></style>
--- client/src/chart-config.js
+++ client/src/chart-config.js
@@ -3,7 +3,7 @@
  * 
  * @param {Forecast} forecast 
  */
-export const getChartConfig = (forecast) => {
+export const getLineChartConfig = (forecast) => {
   return {
     type: 'line',
     data: {
@@ -12,9 +12,61 @@ export const getChartConfig = (forecast) => {
           data: forecast,
           backgroundColor: 'rgba(255, 99, 132, 0.2)',
           borderColor: [
-            // 'rgba(255, 99, 132, 1)',
             'rgba(54, 162, 235, 1)',
-            // 'rgba(153, 102, 255, 1)',
+          ],
+          borderWidth: 2,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: true,
+      scales: {
+        x: {
+          title: {
+            display: true,
+            text: 'Date',
+          },
+        },
+        y: {
+          title: {
+            display: true,
+            text: 'Traffic',
+          },
+        },
+      },
+      parsing: {
+        xAxisKey: 'ds',
+        yAxisKey: 'yhat1',
+      },
+      plugins: {
+        legend: {
+          display: false,
+        },
+        title: {
+          display: true,
+          text: 'Traffic Forecast',
+        },
+      },
+    },
+  };
+}
+
+/**
+ * Get bar chart config object
+ * 
+ * @param {Forecast} forecast 
+ */
+export const getBarChartConfig = (forecast) => {
+  return {
+    type: 'bar',
+    data: {
+      datasets: [
+        {
+          data: forecast,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: [
+            'rgb(255, 99, 132)',
           ],
           borderWidth: 2,
         },
